2c825dd0db0b88caa2f957d026becc60ef356558
codingwithrsn33 <darekarrohan3377@gmail.com>
2025-10-25 22:35:57 +0530

    portfolio

--- Myportfolio/settings.py
+++ Myportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mu7x8%8_ac20hqtd%dz4bila-@_smxd#drr8tc3w+6q5h(=32-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rohandarekar.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
